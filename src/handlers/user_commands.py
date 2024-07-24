64409648848dc7e94fe24e22af17f5a8c86e5cec
DenysTsotsko <denistsotsko@icloud.com>
2024-07-23 23:12:47 -0700

    some changes

--- src/handlers/user_commands.py
+++ src/handlers/user_commands.py
@@ -1,6 +1,6 @@
 from aiogram import Router, F
 from aiogram.types import Message
-from aiogram.filters import Command, CommandStart, F
+from aiogram.filters import Command, CommandStart
 
 from keyboards.reply import main_kb
 
@@ -10,8 +10,3 @@ router = Router()
 @router.message(CommandStart())
 async def start(message: Message): 
     await message.answer(f"Hello, {message.from_user.first_name}", reply_markup=main_kb)
-
-
-@router.message(F.text.lower() == "workout")
-async def echo(message: Message): 
-    await message.answer("Here where the fsm should stay")
